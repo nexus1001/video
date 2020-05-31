301b60351e3531f04a18fe076dc707b5237f229c
nexus1001 <66215931+nexus1001@users.noreply.github.com>
2020-06-01 04:33:25 +0900

    initial commit.
    テスト

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const Peer = window.Peer;
 
   // eslint-disable-next-line require-atomic-updates
   const peer = (window.peer = new Peer({
-    key: '68124ab2-dc3d-452d-99a8-dda0c73bd52f',
+    key: 'e5440a0d-d51b-4519-a1b7-40cf0e222f4b',
     debug: 3,
   }));
 
@@ -118,25 +118,3 @@ const Peer = window.Peer;
 
   peer.on('error', console.error);
 })();
-const toggleCamera = document.getElementById('js-toggle-camera');
-const toggleMicrophone = document.getElementById('js-toggle-microphone');
-const cameraStatus = document.getElementById('camera-status');
-const microphoneStatus = document.getElementById('microphone-status');
-
-toggleCamera.addEventListener('click', () => {
-  const videoTracks = localStream.getVideoTracks()[0];
-  videoTracks.enabled = !videoTracks.enabled;
-  cameraStatus.textContent = `カメラ${videoTracks.enabled ? 'ON' : 'OFF'}`;
-});
-
-toggleMicrophone.addEventListener('click', () => {
-  const audioTracks = localStream.getAudioTracks()[0];
-  audioTracks.enabled = !audioTracks.enabled;
-  microphoneStatus.textContent = `マイク${audioTracks.enabled ? 'ON' : 'OFF'}`;
-});
-const localVideo = document.getElementById('js-local-stream');
-
-localVideo.srcObject = localStream;
-localVideo.muted = true; // 自分の音声を自分のスピーカーから聞こえなくする。相手には届く。
-localVideo.playsInline = true;
-localVideo.autoplay = true;
