6245026c9147f2e857c3d899596ccf857b245bdb
nexus1001 <66215931+nexus1001@users.noreply.github.com>
2020-11-05 11:05:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -164,9 +164,7 @@ navigator.mediaDevices
       // Send message to all of the peers in the room via websocket
       room.send(localText.value);
       //messages.textContent +=localText.value;
-      messages.textContent += localText.value;
-      localText.value =`${nameId}${localText.value}\n`;
-      
+      messages.textContent +=`${localText.value}\n`;
       localText.value ='';
     }
     
